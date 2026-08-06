bcc96554cef45cbe9ad974de9192297b3e68a1ed
Harlan Wilton <harlan@harlanzw.com>
2026-08-06 11:43:31 +1000

    refactor(nuxt): disable component auto imports (#148)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+  components: {
+    dirs: [
+      { path: '~/components/global', global: true },
+      '~/components',
+    ],
+    transform: {
+      exclude: [/\.vue(?:$|\?)/, /\.[jt]sx(?:$|\?)/],
+    },
+  },
   imports: { autoImport: false },
 
   nitro: {
